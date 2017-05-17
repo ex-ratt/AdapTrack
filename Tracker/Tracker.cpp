cd2e0bd64a6f63fd763ce3616dae38e5c313683e
Peter Poschmann <peterposchmann@web.de>
2017-05-17 16:17:13 +0200

    better evaluation of tracking and usage of program arguments

--- Tracker/Tracker.cpp
+++ Tracker/Tracker.cpp
@@ -41,10 +41,10 @@ using namespace tracking;
 using namespace tracking::filtering;
 
 struct Params {
-	string svmFrontal;
-	float thresholdFrontal;
-	string svmAll;
-	float thresholdAll;
+	string svmDetection;
+	float thresholdDetection;
+	string svmTracking;
+	float thresholdTracking;
 	int binCount;
 	int cellSize;
 	int minSize;
@@ -70,18 +70,37 @@ double computeOverlap(Rect a, Rect b);
 
 int main(int argc, char **argv) {
 	// TODO use program arguments or config files
+	string video = argv[1];
+	string trackingSvmFile = argv[2];
+	float initiationThreshold = std::stof(argv[3]);
+	bool adaptive = std::stoi(argv[4]);
+	double terminationThreshold = std::stof(argv[5]);
+
 //	shared_ptr<ImageSource> images = make_shared<CameraImageSource>(0);
+
+	// svm-fhog6-4x8 ab3 0.5 -> 0.00307 (0.00256 - 0.00512)
+	// svm-fhog6-4x8 group 0.75 -> 0.00444
+	Params params{trackingSvmFile, initiationThreshold, trackingSvmFile, 0.0f,
+		std::stoi(trackingSvmFile.substr(8, 1)), std::stoi(trackingSvmFile.substr(10, 1)), 40, 480};
+
+	if (video != "group" && video != "ab3")
+		throw invalid_argument("unknown video: " + video);
+
+	shared_ptr<DlibImageSource> images = video == "group"
+			? make_shared<DlibImageSource>("/home/ex-ratt/Videos/a318/group/annotations.xml")
+			: make_shared<DlibImageSource>("/home/ex-ratt/Videos/filesAb3/annotations.xml");
 
 //	shared_ptr<ImageSource> images = make_shared<DirectoryImageSource>("/home/ex-ratt/Videos/filesAb3/");
 //	shared_ptr<DlibImageSource> images = make_shared<DlibImageSource>("/home/ex-ratt/Videos/filesAb3/annotations.xml");
 //	Params params{"svm-fhog4-5x8", 1.5f, "svm-fp-fhog4-5x8", 0.0f, 4, 5, 50, 480}; // 2.3f
 
 //	shared_ptr<ImageSource> images = make_shared<DirectoryImageSource>("/home/ex-ratt/Videos/a318/A4/");
+//	shared_ptr<DlibImageSource> images = make_shared<DlibImageSource>("/home/ex-ratt/Videos/a318/A4/annotations.xml");
 //	Params params{"svm-fhog4-5x8", 1.5f, "svm-fp-fhog4-5x8", 0.0f, 4, 5, 50, 480};
 
 //	shared_ptr<ImageSource> images = make_shared<DirectoryImageSource>("/home/ex-ratt/Videos/a318/group/");
-	shared_ptr<DlibImageSource> images = make_shared<DlibImageSource>("/home/ex-ratt/Videos/a318/group/annotations.xml");
-	Params params{"svm-fhog4-5x8", 2.2f, "svm-fp-fhog4-5x8", 0.0f, 4, 5, 50, 480};
+//	shared_ptr<DlibImageSource> images = make_shared<DlibImageSource>("/home/ex-ratt/Videos/a318/group/annotations.xml");
+//	Params params{"svm-fhog4-5x8", 2.2f, "svm-fp-fhog4-5x8", 0.0f, 4, 5, 50, 480};
 //	Params params{"svm-fhog4-4x10", 0.8f, "svm-fp-fhog4-4x10", 0.0f, 4, 4, 50, 480};
 
 //	shared_ptr<ImageSource> images = make_shared<DirectoryImageSource>("/home/ex-ratt/Documents/thesis-stuff/log-tdot-03-2013/2013_03_23_10-40-31/uEyeOmni_Image/OmniImage_2013-03-23_10-40-31/");
@@ -96,38 +115,41 @@ int main(int argc, char **argv) {
 //	shared_ptr<ImageSource> images = make_shared<DirectoryImageSource>("/home/ex-ratt/Documents/thesis-stuff/log-2011-lndw/2011_7_1_18-20-55/uEyeOmni_Image/OmniImage_2011-07-01_18-20-55/");
 //	Params params{"svm-fhog4-5x8", 2.5f, "svm-fp-fhog4-5x8", 0.0f, 4, 4, 32, 240};
 
-	shared_ptr<ProbabilisticSvmClassifier> svm = loadSvm(params.svmFrontal, params.thresholdFrontal);
-	shared_ptr<ProbabilisticSvmClassifier> svmAllHeads = loadSvm(params.svmAll, params.thresholdAll);
-	svm->setLogisticA(0.0);
-	svmAllHeads->setLogisticA(0.0);
+	shared_ptr<ProbabilisticSvmClassifier> detectionSvm = loadSvm(params.svmDetection, params.thresholdDetection);
+	shared_ptr<ProbabilisticSvmClassifier> trackingSvm = loadSvm(params.svmTracking, params.thresholdTracking);
+	detectionSvm->setLogisticA(0.0);
+	trackingSvm->setLogisticA(0.0);
 
-	int windowWidth = svm->getSvm()->getSupportVectors()[0].cols;
-	int windowHeight = svm->getSvm()->getSupportVectors()[0].rows;
+	int windowWidth = detectionSvm->getSvm()->getSupportVectors()[0].cols;
+	int windowHeight = detectionSvm->getSvm()->getSupportVectors()[0].rows;
 	shared_ptr<FhogFilter> fhogFilter = createFhogFilter(params.binCount, params.cellSize);
 	shared_ptr<ExactFhogExtractor> exactFhogExtractor = make_shared<ExactFhogExtractor>(fhogFilter, windowWidth, windowHeight);
-	shared_ptr<AggregatedFeaturesDetector> detector = createDetector(fhogFilter, svm->getSvm(), params.cellSize, params.minSize, params.maxSize);
+	shared_ptr<AggregatedFeaturesDetector> detector = createDetector(fhogFilter, detectionSvm->getSvm(), params.cellSize, params.minSize, params.maxSize);
 	shared_ptr<MotionModel> motionModel = make_shared<RandomWalkModel>(0.2, 0.05);
-	unique_ptr<Tracker> headTracker = make_unique<Tracker>(exactFhogExtractor, detector, svmAllHeads, motionModel);
-	headTracker->particleCount = 500;
-	headTracker->adaptive = true;
-	headTracker->associationThreshold = 0.3;
-	headTracker->visibilityThreshold = -0.3;
-	headTracker->negativeExampleCount = 10;
-	headTracker->negativeOverlapThreshold = 0.5;
-	headTracker->targetSvmC = 10;
-	headTracker->learnRate = 0.5;
-	run(*headTracker, *images);
+	unique_ptr<Tracker> tracker = make_unique<Tracker>(exactFhogExtractor, detector, trackingSvm, motionModel);
+	tracker->particleCount = 500;
+	tracker->adaptive = adaptive;
+	tracker->associationThreshold = 0.3;
+	tracker->visibilityThreshold = terminationThreshold;
+	tracker->negativeExampleCount = 10;
+	tracker->negativeOverlapThreshold = 0.5;
+	tracker->targetSvmC = 10;
+	tracker->learnRate = 0.5;
+	run(*tracker, *images);
 //	cout << fixed << setprecision(2);
-//	cout << "group " << params.svmFrontal
-//			<< " threshold=" << params.thresholdFrontal
-//			<< " particles=" << headTracker->particleCount
-//			<< " C=" << headTracker->targetSvmC
-//			<< " negativeOverlap=" << headTracker->negativeOverlapThreshold
-//			<< " (hard)negatives=" << headTracker->negativeExampleCount
-//			<< " learnRate=" << headTracker->learnRate
-//			<< " visibility=" << headTracker->visibilityThreshold
-//			<< endl;
-//	evaluate(*headTracker, *images, static_cast<double>(windowWidth) / windowHeight, 15);
+//	cout << video
+//			<< " particles=" << tracker->particleCount
+//			<< (tracker->adaptive ? " adaptive" : " non-adaptive")
+//			<< " " << params.svmTracking
+//			<< " threshold=" << params.thresholdDetection
+//			<< " visibility=" << tracker->visibilityThreshold;
+//	if (tracker->adaptive)
+//	cout << " negativeOverlap=" << tracker->negativeOverlapThreshold
+//			<< " (hard)negatives=" << tracker->negativeExampleCount
+//			<< " C=" << tracker->targetSvmC
+//			<< " learnRate=" << tracker->learnRate;
+//	cout << endl;
+//	evaluate(*tracker, *images, static_cast<double>(windowWidth) / windowHeight, 25);
 
 	return EXIT_SUCCESS;
 }
@@ -149,7 +171,7 @@ shared_ptr<AggregatedFeaturesDetector> createDetector(
 	int windowWidth = svm->getSupportVectors()[0].cols;
 	int windowHeight = svm->getSupportVectors()[0].rows;
 	shared_ptr<NonMaximumSuppression> nms = make_shared<NonMaximumSuppression>(
-			0.3, NonMaximumSuppression::MaximumType::WEIGHTED_AVERAGE);
+			0.3, NonMaximumSuppression::MaximumType::MAX_SCORE);
 	return make_shared<AggregatedFeaturesDetector>(
 			fhogFilter, cellSize, Size(windowWidth, windowHeight), 5, svm, nms, 1.0, 1.0, minSize, maxSize);
 }
@@ -199,9 +221,11 @@ void run(Tracker& tracker, ImageSource& images) {
 		for (const pair<int, cv::Rect>& target : targets)
 			rectangle(output, target.second, colors[target.first % colors.size()], thickness);
 		imshow("Detections", output);
+
 		iterationTimeSum += iterationTime;
 		float iterationFps = static_cast<double>(frameCount) / iterationTimeSum.count();
 		cout << fixed << setprecision(1);
+		cout << frameCount << ": ";
 		cout << iterationTime.count() << " ms -> ";
 		cout << iterationFps << " fps" << endl;
 		char c = (char)waitKey(pause ? 0 : 2);
@@ -238,23 +262,36 @@ void evaluate(Tracker& tracker, LabeledImageSource& source, double aspectRatio, 
 		images.emplace_back(source.getImage(), source.getLandmarks().getLandmarks());
 	for (LabeledImage& image : images)
 		image.adjustSizes(aspectRatio);
-	TrackingEvaluation result;
+
+	TrackingEvaluation mean{0,0,0,0};
 	vector<TrackingEvaluation> results(count);
 	for (int i = 0; i < count; ++i) {
 		tracker.reset();
 		results[i] = evaluate(tracker, images);
-		result.fppi += results[i].fppi;
-		result.mr += results[i].mr;
-		result.overlapAverage += results[i].overlapAverage;
-		result.iterationFps += results[i].iterationFps;
+		mean.fppi += results[i].fppi;
+		mean.mr += results[i].mr;
+		mean.overlapAverage += results[i].overlapAverage;
+		mean.iterationFps += results[i].iterationFps;
 	}
-	result.fppi /= count;
-	result.mr /= count;
-	result.overlapAverage /= count;
-	result.iterationFps /= count;
+	mean.fppi /= count;
+	mean.mr /= count;
+	mean.overlapAverage /= count;
+	mean.iterationFps /= count;
 
-	cout << fixed << setprecision(3);
-	cout << "Miss rate: " << result.mr << " (";
+	TrackingEvaluation variance{0,0,0,0};
+	for (int i = 0; i < count; ++i) {
+		variance.fppi += (mean.fppi - results[i].fppi) * (mean.fppi - results[i].fppi);
+		variance.mr += (mean.mr - results[i].mr) * (mean.mr - results[i].mr);
+		variance.overlapAverage += (mean.overlapAverage - results[i].overlapAverage) * (mean.overlapAverage - results[i].overlapAverage);
+		variance.iterationFps += (mean.iterationFps - results[i].iterationFps) * (mean.iterationFps - results[i].iterationFps);
+	}
+	variance.fppi /= count;
+	variance.mr /= count;
+	variance.overlapAverage /= count;
+	variance.iterationFps /= count;
+
+	cout << fixed << setprecision(4);
+	cout << "Miss rate: " << mean.mr << "/" << sqrt(variance.mr) << " (";
 	for (int i = 0; i < count; ++i) {
 		if (i > 0)
 			cout << ", ";
@@ -262,7 +299,8 @@ void evaluate(Tracker& tracker, LabeledImageSource& source, double aspectRatio, 
 	}
 	cout << ")" << endl;
 
-	cout << "FPPI: " << result.fppi << " (";
+	cout << fixed << setprecision(5);
+	cout << "FPPI: " << mean.fppi << "/" << sqrt(variance.fppi) << " (";
 	for (int i = 0; i < count; ++i) {
 		if (i > 0)
 			cout << ", ";
@@ -270,7 +308,8 @@ void evaluate(Tracker& tracker, LabeledImageSource& source, double aspectRatio, 
 	}
 	cout << ")" << endl;
 
-	cout << "Avg Overlap: " << result.overlapAverage << " (";
+	cout << fixed << setprecision(3);
+	cout << "Avg Overlap: " << mean.overlapAverage << "/" << sqrt(variance.overlapAverage) << " (";
 	for (int i = 0; i < count; ++i) {
 		if (i > 0)
 			cout << ", ";
@@ -279,7 +318,7 @@ void evaluate(Tracker& tracker, LabeledImageSource& source, double aspectRatio, 
 	cout << ")" << endl;
 
 	cout << std::fixed << std::setprecision(1);
-	cout << "FPS: " << result.iterationFps << " (";
+	cout << "FPS: " << mean.iterationFps << "/" << sqrt(variance.iterationFps) << " (";
 	for (int i = 0; i < count; ++i) {
 		if (i > 0)
 			cout << ", ";
@@ -327,7 +366,7 @@ TrackingEvaluation evaluate(Tracker& tracker, const vector<LabeledImage>& images
 		}
 		falsePositives += targets.size();
 	}
-	TrackingEvaluation result;
+	TrackingEvaluation result{0,0,0,0};
 	result.fppi = falsePositives / static_cast<double>(frames);
 	result.mr = falseNegatives / static_cast<double>(falseNegatives + truePositives);
 	result.overlapAverage = overlapSum / truePositives;
